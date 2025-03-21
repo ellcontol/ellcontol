e9ca852bd0cb3e950e1289a079308e0d014f1768
Sann <aseepparallon@gmail.com>
2025-03-21 16:27:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Toggle Menu
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+}
+
+// Dark Mode Toggle
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    darkModeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
